f5296b1b1ec3a48145b1b5a335fb989e5c7f14a7
Artem Korovkin <AOKorovkin@sberbank.ru>
2023-07-11 16:03:22 +0300

    Initial Commit

---
+++ src/main/java/ru/kao/kaonotebackend/KaoNoteBackEndApplication.java
@@ -0,0 +1,13 @@
+package ru.kao.kaonotebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KaoNoteBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KaoNoteBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/kao/kaonotebackend/KaoNoteBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.kao.kaonotebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KaoNoteBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
